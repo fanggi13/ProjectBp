0aa23dbecad6e77ed0c0d11f0693163d17c8d0da
amikom <fanggiandrian13@students.amikom.ac.id>
2023-09-27 16:17:34 +0700

    update

---
+++ app/src/main/java/com/example/projectbp/AboutActivity.kt
@@ -0,0 +1,17 @@
+package com.example.projectbp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+        val imgCall:ImageView = findViewById<ImageView?>(R.id.imageView3).let {
+           number->Intent ()
+        }
+        startActivity(callIntent)
+    }
+}
