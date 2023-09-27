f5bdcebb49582f045a07a73d2694493cb86b11ff
amikom <fanggiandrian13@students.amikom.ac.id>
2023-09-27 17:07:12 +0700

    update

--- app/src/main/java/com/example/projectbp/LoginActivity.kt
+++ app/src/main/java/com/example/projectbp/LoginActivity.kt
@@ -12,24 +12,26 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         //instance
-        val btnLogin:Button = findViewById(R.id.buttonLogin)
-        val txtUsername:EditText = findViewById(R.id.editTextUsername)
-        val txtPassword:EditText = findViewById(R.id.editTextPassword)
-        val logModel = LoginModel ()
+        val btnLogin: Button = findViewById(R.id.buttonLogin)
+        val txtUsername: EditText = findViewById(R.id.editTextUsername)
+        val txtPassword: EditText = findViewById(R.id.editTextPassword)
+        val logModel = LoginModel()
         //event btn login di-klik
         btnLogin.setOnClickListener {
             logModel.username = txtUsername.text.toString()
             logModel.password = txtPassword.text.toString()
             //validasi login
             if (logModel.logincek() == true) {
-             {
-                //call home activity
-                val intentHome = Intent(this, HomeActivity::class.java)
-                startActivity(intentHome)
-            } else {
-                Toast.makeText(this, "Userame/Password salah", Toast.LENGTH_SHORT).show()
+
+                    //call home activity
+                    val intentHome = Intent(this, HomeActivity::class.java)
+                    startActivity(intentHome)
+
+                } else {
+                    Toast.makeText(this, "Userame/Password salah", Toast.LENGTH_SHORT).show()
+                }
+
             }
 
-        }
     }
 }
